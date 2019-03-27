c545a61229cdaacf69c2dedbb172a06c205ad61a
Enrique <ebermudez.ing@gmail.com>
2019-03-27 14:56:51 +0100

    Document Coordinator files

--- LightMVVMC/LightMVVMC/Coordinator/Coordinator.swift
+++ LightMVVMC/LightMVVMC/Coordinator/Coordinator.swift
@@ -8,25 +8,39 @@
 
 import UIKit
 
+/// `Presentable` instances are instances that can be presented by a `Coordinator`.
 public protocol Presentable { }
 
 extension UIViewController: Presentable {}
 
+/// `Coordinator` is an instance that can navigates to diferent routes.
 public class Coordinator<CoordinatorRoute: Route> {
     
+    /// Coordinator's starting point.
     public var root: Presentable!
     
+    ///
     init(root: Presentable) {
         self.root = root
     }
     
+    /**
+     Tells the `Coordinator` instance to navigate to a certain route.
+     The `Coordinator` will first get the `Transition` of the `Route` and then it will execute that `Transition`.
+     - parameter destination: Destination `Route`.
+     */
     public func navigate(to destination: CoordinatorRoute) {
         let transition = destination.getTransition()
         transition.execute(from: self.root)
     }
 }
 
-protocol CoordinatorManager {
+///`CoordinatorManager` is an instance that contains its own `Coordinator`.
+public protocol CoordinatorManager {
+    
+    /// :nodoc:
     associatedtype CoordinatorType: Route
+    
+    /// CoordinatorManager's coordinator instance.
     var coordinator: Coordinator<CoordinatorType>! { get set }
 }
